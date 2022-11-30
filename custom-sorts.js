fc8e14a0df5cf06936eb7fd266cecd62e617b79e
kutun0901 <tuan.tq2433@gmail.com>
2022-11-30 13:14:04 -0800

    oddEvenSort done

--- custom-sorts.js
+++ custom-sorts.js
@@ -5,10 +5,16 @@ function ageSort(users) {
 
 function oddEvenSort(arr) {
   // Your code here
+  return arr.sort((a, b) => {
+    if (a % 2 === 0 && b % 2 !== 0) return 1;
+    if (b % 2 === 0 && a % 2 !== 0) return -1;
+    return a - b;
+  })
 }
 
 function validAnagrams(s, t) {
   // Your code here
+
 }
 
 function reverseBaseSort(arr) {
